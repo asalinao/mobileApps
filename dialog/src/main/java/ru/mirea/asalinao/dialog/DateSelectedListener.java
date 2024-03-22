af6dd7e5ebbfa7b93c8b811b200049a856bde3e6
iassetOtomo <solofunch@gmail.com>
2024-03-22 15:26:30 +0300

    first commit

---
+++ dialog/src/main/java/ru/mirea/asalinao/dialog/DateSelectedListener.java
@@ -0,0 +1,5 @@
+package ru.mirea.asalinao.dialog;
+
+public interface DateSelectedListener {
+    void onDateSelected(String date);
+}
---
+++ dialog/src/main/java/ru/mirea/asalinao/dialog/TimeSelectedListener.java
@@ -0,0 +1,5 @@
+package ru.mirea.asalinao.dialog;
+
+public interface TimeSelectedListener {
+    void onTimeSelected(String time);
+}
