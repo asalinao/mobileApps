6d6ade724ebd4242be6654b1470f8caa2f349686
prostodyraa a <solofunch@gmail.com>
2024-05-23 23:20:58 +0300

    first commit

---
+++ yandexdriver/src/main/java/ru/mirea/asalinao/yandexdriver/App.java
@@ -0,0 +1,15 @@
+package ru.mirea.asalinao.yandexdriver;
+
+import android.app.Application;
+
+import com.yandex.mapkit.MapKitFactory;
+
+public class App extends Application {
+    private final String API_KEY = "f51ffcbd-5e90-4e9e-a3aa-4fefc08d5f4d";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MapKitFactory.setApiKey(API_KEY);
+    }
+}
